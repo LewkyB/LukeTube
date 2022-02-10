4312d68df8e20c70cb6be4fe5d925f2bf5eb2a33
luke brown <luke92brown@gmail.com>
2022-02-09 21:53:22 -0600

    partially completed autocomplete from angular materials

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,25 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { Subreddit } from './services/subreddit.service';
-import SubredditView from './views/subredditView.component';
-import { YouTubePlayerModule } from '@angular/youtube-player';
+import { AppComponent } from "./app.component";
+import { Subreddit } from "./services/subreddit.service";
+import SubredditView from "./views/subredditView.component";
+import { YouTubePlayerModule } from "@angular/youtube-player";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
-  declarations: [
-        AppComponent,
-        SubredditView,
-  ],
+  declarations: [AppComponent, SubredditView],
   imports: [
-      BrowserModule,
-      HttpClientModule,
-      YouTubePlayerModule,
+    BrowserModule,
+    HttpClientModule,
+    YouTubePlayerModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
   ],
-    providers: [
-        Subreddit
-    ],
-  bootstrap: [AppComponent]
+  providers: [Subreddit],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/views/subredditView.component.ts
+++ client/src/app/views/subredditView.component.ts
@@ -1,24 +1,38 @@
 ﻿import { OnInit } from "@angular/core";
 import { Component } from "@angular/core";
 import { Subreddit } from "../services/subreddit.service";
+import { FormControl } from "@angular/forms";
+import { NEVER, Observable } from "rxjs";
+import { map, startWith } from "rxjs/operators";
 
 @Component({
-    selector: "subreddit-links",
-    templateUrl: "subredditView.component.html",
+  selector: "subreddit-links",
+  templateUrl: "subredditView.component.html",
 })
-export default class subredditView implements OnInit{
+export default class subredditView implements OnInit {
+  constructor(public subreddit: Subreddit) {}
 
-    constructor(public subreddit: Subreddit) {
-    }
+  myControl = new FormControl("");
 
-    ngOnInit(): void {
-        // this.subreddit.loadComments()
-        //     .subscribe(() => {
-        //         // do something
-        //     });
-        this.subreddit.loadSubreddits()
-            .subscribe(() => {
-                // do something
-            });
-    }
+  subreddits!: string[];
+  filteredSubreddits!: Observable<string[]>;
+
+  ngOnInit(): void {
+    this.subreddit.loadSubreddits().subscribe(() => {
+      // do something
+    });
+
+    this.filteredSubreddits = this.myControl.valueChanges.pipe(
+      startWith(""),
+      map((value) => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.subreddits.filter((subreddit) =>
+      subreddit.toLowerCase().includes(filterValue)
+    );
+  }
 }
